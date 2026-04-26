85a7ac1d217f034541695e328b5174fce60cc537
Xuanyu Quah <xquah@u90z04s03.42kl.edu.my>
2026-04-26 18:29:46 +0800

    changed main again

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,6 @@ int	main(int argc, char **argv)
 	(void)argv;
 	if (argc == 1)
 	{
-		/* [ SPECIAL TEST ] */
 		std::cout << "[ SPECIAL TEST ]\n";
 		{
 			try
@@ -24,13 +23,6 @@ int	main(int argc, char **argv)
 			}
 		}
 
-		/** 
-		 * [ CHAR TEST ]
-		 * -128-31, 127, others: Non displayable
-		 * 32-126: Printable
-		 * Note: Casting an int that's too large or too small to char will result
-		 *       in overflow. E.g., 128 will overflow to -128.
-		 * */
 		std::cout << "[ CHAR TEST ]\n";
 		{
 			for (int i = -128; i < 128; ++i)
@@ -49,25 +41,10 @@ int	main(int argc, char **argv)
 			}
 		}
 
-		/**
-		 * [ INT TEST ]
-		 * CHAR - See above with the addition of 'Impossible' if out of range.
-		 * INT - Anything from -2147483648 to 2147483647. Others: Over/Underflow
-		 * FLOAT/DOUBLE - All printable. If INT over/underflows, give +/-inf(f)
-		 */
 		std::cout << "[ INT TEST ]\n";
 		{
 			try
 			{
-				// std::stringstream	ss;
-				// for (int i = -10; i < 11; ++i)
-				// {
-				// 	ss.str("");
-				// 	ss.clear();
-				// 	ss << i;
-				// 	ScalarConverter::convert(ss.str());
-				// 	std::cout << "\n";
-				// }
 				ScalarConverter::convert("-123"); std::cout << "\n";
 				ScalarConverter::convert("123");std::cout << "\n";
 				ScalarConverter::convert("0"); std::cout << "\n";
@@ -82,35 +59,10 @@ int	main(int argc, char **argv)
 			}
 		}
 
-		/**
-		 * [ FLOAT TEST ]
-		 * Type casting removes the decimal places for int and char: equivalent to floor()
-		 * +-3.40282347e+38f; +-1.17549435e-38f
-		 * */ 
 		{
 			std::cout << "[ FLOAT TEST ]\n";
 			try
 			{
-				// std::stringstream ss;
-				// std::cout << std::endl;
-				// for (float i = 1.0f; i < 1.1f; i += 0.00001f)
-				// {
-				// 	std::cout << "Actual float: " << i << "f\n";
-				// 	ss.str("");
-				// 	ss.clear();
-				// 	ss << i;
-				// 	ScalarConverter::convert(ss.str() + "f");
-				// 	std::cout << "\n";
-				// }
-				// for (float i = -1.0f; i <= 2.0f; i += 0.1f)
-				// {
-				// 	std::cout << "Actual float: " << i << "f\n";
-				// 	ss.str("");
-				// 	ss.clear();
-				// 	ss << i;
-				// 	ScalarConverter::convert(ss.str() + "f");
-				// 	std::cout << "\n";
-				// }
 				ScalarConverter::convert("123.0f");
 				std::cout << "\n";
 				ScalarConverter::convert("-123.0f");
@@ -152,24 +104,10 @@ int	main(int argc, char **argv)
 			
 		}
 
-		/**
-		 * [ DOUBLE TEST ]
-		 * +-1.7976931348623157e+308; +-2.2250738585072014e-308
-		 */
 		std::cout << "[ DOUBLE TEST ]\n";
 		{
 			try
 			{
-				// std::stringstream	ss;
-				// for (double i = -2.0; i <= 2.0; i += 0.1)
-				// {
-				// 	std::cout << "Actual double: " << i << "\n";
-				// 	ss.str("");
-				// 	ss.clear();
-				// 	ss << i;
-				// 	ScalarConverter::convert(ss.str());
-				// 	std::cout << "\n";
-				// }
 				ScalarConverter::convert("123.0");
 				std::cout << "\n";
 				ScalarConverter::convert("-123.0");
@@ -236,7 +174,6 @@ int	main(int argc, char **argv)
 	
 	if (argc == 2)
 	{
-		// ScalarConverter test; // cannot be instantiated
 		try
 		{
 			ScalarConverter::convert(argv[1]);
